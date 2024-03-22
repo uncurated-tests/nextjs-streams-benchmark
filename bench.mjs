2a77e04d06bdc2a1f7ad8fe5a268f9dc57178501
Ethan Arrowood <ethan@arrowood.dev>
2024-03-22 16:58:47 -0600

    improve bench

--- bench.mjs
+++ bench.mjs
@@ -1,21 +1,44 @@
 import { parseArgs } from 'node:util';
-import packageJSON from './package.json' assert { type: 'json' };
+import fs from 'node:fs';
+
+const packageJSON = JSON.parse(fs.readFileSync('./package.json'));
 
 const options = {
     port: {
         type: 'string',
-        short: 'p'
+        short: 'p',
+        default: '3000'
     },
-    endpoints: {
+    endpoint: {
         type: 'string',
-        multiple: true,
-        short: 'e',
+        short: 'e'
+    },
+    log: {
+        type: 'boolean',
+        short: 'l',
+        default: false
+    },
+    iterations: {
+        type: 'string',
+        short: 'i',
+        default: '10'
+    },
+    output: {
+        type: 'string',
+        short: 'o'
     }
 }
+
 const { values } = parseArgs({ options })
 
-async function run (endpoint) {
-    const res = await fetch(`http://localhost:${values.port}/${endpoint}`);
+function average (measures) {
+    let ttl = 0;
+    measures.forEach(measure => ttl += measure.duration);
+    return ttl / measures.length;
+}
+
+async function run () {
+    const res = await fetch(`http://localhost:${values.port}/${values.endpoint}`);
     performance.mark('streaming-start');
     const text = await res.text();
     performance.mark('streaming-end');
@@ -25,46 +48,54 @@ async function run (endpoint) {
     });
 }
 
-function average (measures) {
-    let ttl = 0;
-    measures.forEach(measure => ttl += measure.duration);
-    return ttl / measures.length;
-}
-
 function clear () {
     performance.clearMarks('streaming-start');
     performance.clearMarks('streaming-end');
     performance.clearMeasures('streaming-duration');
 }
 
-async function bench () {
-    console.log('Benchmarking Next.js version', packageJSON.dependencies.next)
-
-    for (const endpoint of values.endpoints) {
-        console.log('Endpoint', endpoint);
-        const runs = [];
-        for (let i = 0; i < 100; i++) {
-            runs.push(run(endpoint));
-        }
-        await Promise.all(runs);
-
-        const measures = performance.getEntriesByName('streaming-duration');
-
-        console.log('Average duration', average(measures))
-
-        clear()
-    }
-}
-
-const warmups = []
-for (const endpoint of values.endpoints) {
+async function warmup () {
+    const warmups = []
     for (let i = 0; i < 5; i++) {
-        warmups.push(run(endpoint));
+        warmups.push(run());
     }
+    await Promise.all(warmups);
+    clear();
 }
 
-await Promise.all(warmups);
+async function bench () {
+    const runs = [];
+    for (let i = 0; i < 100; i++) {
+        runs.push(run());
+    }
+    await Promise.all(runs);
 
-clear();
+    const measures = performance.getEntriesByName('streaming-duration');
 
-await bench();
+    const averageDuration = average(measures);
+
+    clear();
+
+    return { endpoint: values.endpoint, average_duration: averageDuration };
+}
+
+if (values.log) {
+    console.log('Benchmarking Next.js version', packageJSON.dependencies.next)
+    console.log('Endpoint', values.endpoint)
+}
+await warmup();
+const output = values.output ?? `nextjs-streams-benchmark-${values.endpoint}-${Date.now()}`;
+const results = [];
+for (let i = 0; i < parseInt(values.iterations); i++) {
+    const result = await bench();
+    results.push(result);
+}
+
+let t = 0;
+results.forEach(({ average_duration }) => t += average_duration);
+const outString = `Average duration for ${values.endpoint} after ${values.iterations} iterations of 100 runs: ${t/results.length}ms`;
+if (values.log) { 
+    console.log(outString)
+} else {
+    fs.writeFileSync(output, outString);
+}
